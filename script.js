6443009c4f46aafa60dd4bc8154d0f06cb46165b
MehmetAlpYS <166522020+MehmetAlpYS@users.noreply.github.com>
2024-04-29 16:59:46 +0200

    2ème commit
    Finalisation de la partie Réalisation
    Début de la partie Témoignage

--- script.js
+++ script.js
@@ -17,15 +17,17 @@ function addProducts(data) {
   console.log(data);
 
 
+  // Lecture du tableau Réalisation 
     let real=""
     data.realisations.forEach(element => {
+      // Affectation des clés sur les balises HTML
         real += `<div class="card"> <h3>${element.nom}</h3> <img src="${element.image}" alt=""> <p>${element.description}</p> </div>`
     });
     console.log(real)
 
     let temoi=""
     data.temoignages.forEach(element => {
-        temoi += `<div class="card2"> <p>${element.prenom}</p> <p>${element.typePrestation}</p> <p>${element.commentaire}</p>`
+        temoi += `<div class="card2"> <h3>${element.prenom}</p> <h4>${element.typePrestation}</p> <p>${element.commentaire}</p> <p class="note">${element.note}</p> </div>`
     });
     console.log(temoi)
     let list=""
@@ -34,19 +36,19 @@ function addProducts(data) {
     });
     console.log(list)
 
-
-    document.querySelector("#content").innerHTML += real
-
-   // document.querySelector("#content2").innerHTML += temoi
-
-   document.querySelector("#slog").innerHTML +=`
+    // Affectation de la section hero
+    document.querySelector("#slog").innerHTML +=`
     <h2>${data.nomCommercial}</h2> <br> <br>
     <p>${data.texteBouton}</p> <br>
     <p>${data.phraseAccroche}</p>  <br>
     <p>${data.listeBenefficesClients}</p>
    `
 
-  
+   // Partie Réalisation 
+    document.querySelector("#content").innerHTML += real
+    //Partie Témoignage 
+    document.querySelector("#content2").innerHTML += temoi
+
 }
 
 
